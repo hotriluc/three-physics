052ba5fee4908539dc6af3041d5bb36ce77e79af
Luc Ho <hotriluc97@gmail.com>
2022-10-01 18:36:28 +0200

    threejs with physics

---
+++ src/script.js
@@ -0,0 +1,279 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "lil-gui";
+import CANNON, { Vec3 } from "cannon";
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI();
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader();
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+
+const environmentMapTexture = cubeTextureLoader.load([
+  "/textures/environmentMaps/0/px.png",
+  "/textures/environmentMaps/0/nx.png",
+  "/textures/environmentMaps/0/py.png",
+  "/textures/environmentMaps/0/ny.png",
+  "/textures/environmentMaps/0/pz.png",
+  "/textures/environmentMaps/0/nz.png",
+]);
+
+const geometry = new THREE.SphereGeometry(1, 20, 20);
+const material = new THREE.MeshStandardMaterial({
+  metalness: 1,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+});
+
+gui.add(material, "metalness", 0, 1, 0.001);
+gui.add(material, "roughness", 0, 1, 0.001);
+
+/**
+ * Physics
+ */
+// WORLD
+const world = new CANNON.World();
+world.gravity.set(0, -9.82, 0);
+
+// Materials
+const defaultMaterial = new CANNON.Material("default");
+const defaultContactMaterial = new CANNON.ContactMaterial(
+  defaultMaterial,
+  defaultMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.7,
+  }
+);
+world.addContactMaterial(defaultContactMaterial);
+
+// Shapes
+const floorShape = new CANNON.Plane();
+const floorBody = new CANNON.Body({
+  mass: 0,
+  shape: floorShape,
+  material: defaultMaterial,
+});
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI / 2);
+
+const boxShape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5));
+const boxBody = new CANNON.Body({
+  mass: 0,
+  shape: boxShape,
+  material: defaultMaterial,
+});
+boxBody.position.y = 0.5;
+world.addBody(boxBody);
+world.addBody(floorBody);
+
+/**
+ * Floor
+ */
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshStandardMaterial({
+    color: "#777777",
+    metalness: 0.3,
+    roughness: 0.4,
+    envMap: environmentMapTexture,
+    envMapIntensity: 0.5,
+  })
+);
+floor.receiveShadow = true;
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.camera.left = -7;
+directionalLight.shadow.camera.top = 7;
+directionalLight.shadow.camera.right = 7;
+directionalLight.shadow.camera.bottom = -7;
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(-3, 3, 3);
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**Utils */
+const objects = [];
+
+// BOX
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMesh = new THREE.Mesh(boxGeometry, material);
+boxMesh.position.y = 0.5;
+boxMesh.castShadow = true;
+boxMesh.receiveShadow = true;
+scene.add(boxMesh);
+
+const createSphere = (radius, position) => {
+  const ownMaterial = material.clone();
+  ownMaterial.color.setHex(Math.random() * "0xffffff");
+  const mesh = new THREE.Mesh(geometry, ownMaterial);
+  mesh.scale.set(radius, radius, radius);
+  mesh.castShadow = true;
+  mesh.position.copy(position);
+  scene.add(mesh);
+
+  const sphereShape = new CANNON.Sphere(radius);
+  const sphereBody = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(position.x, position.y, position.z),
+    shape: sphereShape,
+    material: defaultMaterial,
+  });
+  world.addBody(sphereBody);
+
+  return { threeObject: mesh, physicObject: sphereBody };
+};
+
+const createBoxes = (height, width, depth, position) => {
+  const ownMaterial = material.clone();
+  ownMaterial.color.setHex(Math.random() * "0xffffff");
+  const mesh = new THREE.Mesh(boxGeometry, ownMaterial);
+  mesh.scale.set(height, width, depth);
+  mesh.castShadow = true;
+  mesh.position.copy(position);
+  scene.add(mesh);
+
+  const shape = new CANNON.Box(
+    new CANNON.Vec3(height / 2, width / 2, depth / 2)
+  );
+  const body = new CANNON.Body({
+    mass: 5,
+    position: new CANNON.Vec3(position.x, position.y, position.z),
+    shape: shape,
+    material: defaultMaterial,
+  });
+  world.addBody(body);
+
+  return { threeObject: mesh, physicObject: body };
+};
+
+const debugObj = {};
+debugObj.createSphere = () => {
+  objects.push(
+    createSphere(0.5, {
+      x: (Math.random() - 0.5) * 3,
+      y: 3,
+      z: (Math.random() - 0.5) * 3,
+    })
+  );
+};
+gui.add(debugObj, "createSphere");
+
+debugObj.createBoxes = () => {
+  objects.push(
+    createBoxes(Math.random(), Math.random(), Math.random(), {
+      x: (Math.random() - 0.5) * 3,
+      y: 3,
+      z: (Math.random() - 0.5) * 3,
+    })
+  );
+};
+gui.add(debugObj, "createBoxes");
+
+// objects.push(
+//   createSphere(0.5, { x: 0, y: 3, z: 0 }),
+//   createSphere(0.5, { x: 3, y: 3, z: 0 }),
+//   createSphere(0.5, { x: -3, y: 3, z: 0 })
+// );
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+let prevTime = 0;
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - prevTime;
+  prevTime = elapsedTime;
+
+  //update physics world
+  objects.forEach((object) => {
+    object.threeObject.position.copy(object.physicObject.position);
+    object.threeObject.quaternion.copy(object.physicObject.quaternion);
+  });
+  world.step(1 / 60, deltaTime, 3);
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
